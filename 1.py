36cc93d27b84e057e63c4f99557cfcc796c59ac6
张乾乾 <zhangqianqian46@huawei.com>
2024-09-22 14:44:34 +0800

    update

--- 1.py
+++ 1.py
@@ -19,3 +19,33 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision.utils import save_image
 from torchvision import datasets
+
+def nms(bboxes, scores, threshold):
+    x1 = bboxes[:, 0]
+    y1 = bboxes[:, 1]
+    x2 = bboxes[:, 2]
+    y2 = bboxes[:, 3]
+    areas = (x2 - x1) * (y2 - y1)
+    _, order = scores.sort(0, descending=True)
+    keep = []
+    while order.numel() > 0:
+        if order.numel() == 1:
+            i = order.item()
+            keep.append(i)
+            break
+        else:
+            i = order[0].item()
+            keep.append(i)
+        xx1 = x1[order[1:]].clamp(min=x1[i])
+        yy1 = y1[order[1:]].clamp(min=y1[i])
+        xx2 = x2[order[1:]].clamp(max=x2[i])
+        yy2 = y2[order[1:]].clamp(max=y2[i])
+        w = (xx2 - xx1).clamp(min=0)
+        h = (yy2 - yy1).clamp(min=0)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        ids = (ovr <= threshold).nonzero().squeeze()
+        if ids.numel() == 0:
+            break
+        order = order[ids + 1]
+    return torch.LongTensor(keep)
